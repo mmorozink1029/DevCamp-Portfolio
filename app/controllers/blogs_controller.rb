e5be91ae6aefc28dd25ed3a0244ea9f5f9f543df
mmorozink1029 <mattmorozink@gmail.com>
2018-01-29 16:15:30 -0700

    Added nav bar and added default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,11 +5,14 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
@@ -66,9 +69,9 @@ class BlogsController < ApplicationController
       @blog.published!
     elsif @blog.published?
       @blog.draft!
-  end
-
-    redirect_to blogs_url, notice: 'Post status has been updated.'
+    end
+   redirect_to blogs_url, notice: 'Post status has been updated.'
+ end
       
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -3,9 +3,13 @@ module CurrentUserConcern
 
   def current_user
     super || guest_user
+  end
 
 
   def guest_user
-    super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@exmple.com")
+    OpenStruct.new(name: "Guest User", 
+                  first_name: "Guest", 
+                  last_name: "User", 
+                  email: "guest@exmple.com")
   end
 end
